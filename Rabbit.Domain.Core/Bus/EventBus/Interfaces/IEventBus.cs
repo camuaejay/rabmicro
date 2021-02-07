a4334e0af735b65a37e4fa6c129b916a08b92797
Ejay Candid C. Camua <ejaycamua2015@gmail.com>
2021-02-07 23:17:04 +0800

    Initial Commit Domain Event Bus

---
+++ Rabbit.Domain.Core/Bus/EventBus/Interfaces/IEventBus.cs
@@ -0,0 +1,18 @@
+﻿namespace Rabbit.Domain.Core.Bus.EventBus.Interfaces
+{
+    using System.Threading.Tasks;
+    using Rabbit.Domain.Core.Commands.Base;
+    using Rabbit.Domain.Core.Events.Base;
+    using Rabbit.Domain.Core.Events.Interfaces;
+
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+
+        void Publish<T>(T @event) where T : Event;
+
+        void Subscribe<T, TH>() 
+            where T : Event 
+            where TH : IEventHandler<T>;
+    }
+}
---
+++ Rabbit.Domain.Core/Events/Interfaces/IEventHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Rabbit.Domain.Core.Events.Interfaces
+{
+    using Rabbit.Domain.Core.Events.Base;
+    using System.Threading.Tasks;
+
+    public interface IEventHandler<in TEvent> : IEventHandler where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler 
+    { 
+    }
+}
